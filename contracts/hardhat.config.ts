d7effe00db26490f2924be8808504cf86663203a
ageraaniska <agraaniska@gmail.com>
2024-08-18 18:32:58 +0700

    add env example on smartcontract

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,17 +2,14 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import * as dotenv from "dotenv";
 dotenv.config();
-import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-verify";
 import "hardhat-gas-reporter";
 
-
-
 const config: HardhatUserConfig = {
   mocha: {
-    timeout: 0
+    timeout: 0,
   },
   solidity: {
     compilers: [
@@ -24,44 +21,27 @@ const config: HardhatUserConfig = {
             runs: 200,
           },
         },
-      }
+      },
     ],
   },
-  
   networks: {
-    
     hardhat: {
       chainId: 31337,
-      allowUnlimitedContractSize: true
+      allowUnlimitedContractSize: true,
     },
     polygon_amoy: {
-      url: "https://80002.rpc.thirdweb.com/564c9ad6b012ea34b7d44b5edc9bd6ef",
+      url: `https://80002.rpc.thirdweb.com/${process.env.THIRDWEB_API_KEY}`,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    }
-    // mumbaipolygon: {
-    //   url: process.env.MUMBAI_POLYGON_TESTNET_URL || "",
-    //   accounts:
-    //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    // },
-    // bsctestnet: {
-    //   url: process.env.BSC_TESTNET_URL || "",
-    //   accounts:
-    //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    // },
-    
+    },
   },
   etherscan: {
-    // apiKey: {
-    //   polygonMumbai: process.env.POLYGON_API_KEY as string,
-    //   bscTestnet: process.env.BSC_API_KEY as string,
-    // },
-    apiKey: process.env.POLYGONSCAN_API_KEY
+    apiKey: process.env.POLYGONSCAN_API_KEY,
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
+    artifacts: "./artifacts",
   },
 };
 
